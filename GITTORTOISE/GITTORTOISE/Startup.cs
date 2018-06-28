0f918f9db732a73bd58339cfc394d16211881eed
csoni31 <chandrashekharsoni31@gmail.com>
2018-06-28 19:36:04 +0530

    first commit

---
+++ GITTORTOISE/GITTORTOISE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GITTORTOISE.Startup))]
+namespace GITTORTOISE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
